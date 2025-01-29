a878b035fd140d46780b6c00907c1fcaf667acc6
RoberGF <rgfrasta@gmail.com>
2025-01-29 18:40:36 +0100

    testing prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import TweetsPage from "./pages/tweets/tweetsPage"
+import TweetsPage from './pages/tweets/tweetsPage'
 function App() {
- 
-  return <TweetsPage/>
-  
+    return <TweetsPage />
 }
 
 export default App
